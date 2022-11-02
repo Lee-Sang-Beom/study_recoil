538b5bd8c8ae44a86dcaf5f8b231ebc3fdff542e
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-02 10:11:55 +0900

    reverse -> Return to the course: practice todolist

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
+import { BrowserRouter , Route, Routes } from "react-router-dom";
 import "./App.css";
-
-// selector가 반환하는 값을 사용하기 위해, useRecoilValue hook을 import한다.
-import { useRecoilState, useRecoilValue } from "recoil";
-
-// textSelector라는 이름의 selector를 import해야 사용할 수 있다.
-import { minuteState, hourSelector } from "./store/index.js";
+import Home from "./pages/Home/Home";
 
 function App() {
-
-	// atoms
-  const [minutes, setMinutes] = useRecoilState(minuteState); 
-
-  // useRecoilValue hook에 selector를 전달하면,
-  // 현재 atom 상태값을 기준으로, get함수의 실행 결과인 return값을 전달받을 수 있다.
-  const getMinutes = useRecoilValue(hourSelector); 
-
-	/* 
-    selector에서 set 속성을 설정하면, 쓰기 가능한 상태(setHours)를 반환한다.
-    이것으로, selector 내부에서, atoms 상태를 변경시킬 수 있다.
-
-    예제에서는, setHour에 특정 값을 전달하면, selector 내부의 set함수에서 
-    전달된 값을 가공하여 atoms로 전환할 수 있도록 한다.
-
-    selector의 쓰기 가능한 상태를 이용하기 위해서는, selector를
-    useRecoilValue가 아니라 useRecoilState에 전달하면 된다.
-    그럼 쓰기 가능한 상태를 반환한다.
-  */ 
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onMinuteChange = (event) => {
-    const { value } = event.target;
-    setMinutes(value);
-  };
-
-  const onHoursChange = (event) => {
-    const { value } = event.target;
-    setHours(value);
-  };
-
-	console.log(getMinutes); // selector내 get()으로 return된 값 출력
   return (
     <>
-      <div>
-        <input
-          onChange={onMinuteChange}
-          value={minutes}
-          type="number"
-          placeholder="Minutes"
-        />
-        <input
-          value={hours.toFixed(0)}
-          onChange={onHoursChange}
-          type="number"
-          placeholder="Hours"
-        />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,13 @@
-import { atom, selector } from "recoil"; // import selector
+import { atom, selector } from "recoil";
 
-export const minuteState = atom({
-  key: "minutes",
-  default: 0,
+// todoList가 담길 배열
+export const textListAtom = atom({
+  key: "textListAtom", // unique ID
+  default: [], // default value
 });
 
-export const hourSelector = selector({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minuteState) / 60;
-    return minutes;
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-  },
+// todoList의 각 내용이 담김
+export const textAtom = atom({
+  key: "textAtom", // unique ID
+  default: "", // default value
 });
