d62b46c6875188b59bbb9168af684bdb6d620cd7
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-06 08:25:07 +0900

    add inputRef, Autofocus when clicking the edit button

--- src/components/Todolist/Todolist.jsx
+++ src/components/Todolist/Todolist.jsx
@@ -2,6 +2,7 @@ import { textListAtom, textAtom } from "../../store";
 import { useRecoilState } from "recoil";
 import { useRef, useState } from "react";
 import useOnClickOutside from "../../hook/useOnClickOutside.js";
+import { useEffect } from "react";
 
 // todoItem : todoList에서 입력한 내용을 출력해줌
 function TodoItem({ todo }) {
@@ -10,9 +11,10 @@ function TodoItem({ todo }) {
   const textListidx = textList.findIndex((text) => text.id === todo.id);
 
   // notify를 위한 input에서 focus 벗어남 발생 시, 자동으로 edit mode 종료
-  const inputRef = useRef(false);
-  useOnClickOutside(inputRef, () => setIsEdit(false));
-  
+  const ref = useRef(false);
+  const inputRef = useRef(null);
+  useOnClickOutside(ref, () => setIsEdit(false));
+
   const onChange = (e) => {
     const { value } = e.target;
 
@@ -25,13 +27,24 @@ function TodoItem({ todo }) {
     setIsEdit(true);
   };
 
+  useEffect(() => {
+    if (isEdit) {
+      // 수정버튼 click 시, 자동으로 focus생성
+      // handleEditMode() 이벤트 발생 시 호출하면 오류발생 : inputRef의 태그가 아직 만들어지지 않았는데, 접근하려 했기 때문
+      inputRef ? inputRef.current.focus() : alert('inputRef is null!');
+    }
+  }, [isEdit]);
+
   return (
-    <div ref={inputRef}>
-      {!isEdit && (<li style={{ margin:"0.5rem 1rem"}}>
-        <span>{todo.text}</span>
-        <button onClick={handleEditMode}>수정</button>
-        <button>삭제</button>
-      </li>)}
+    // 해당 영역에서 focus 벗어나면 이벤트발생하도록 ref={ref} 지정
+    <div ref={ref}>
+      {!isEdit && (
+        <li style={{ margin: "0.5rem 1rem" }}>
+          <span>{todo.text}</span>
+          <button onClick={handleEditMode}>수정</button>
+          <button>삭제</button>
+        </li>
+      )}
       {isEdit && <input ref={inputRef} value={todo.text} onChange={onChange} />}
     </div>
   );
